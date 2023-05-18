988992d0b9b7a6be1dd156a13bef9862958c7fde
eferreiradel <cont.limaferreira.everton@gmail.com>
2023-05-18 21:21:10 +0200

    feat: Hello Vite

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="v-card">
+    <subtitle></subtitle>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+import subtitle from "./components/subtitle.vue";
+export default {
+  name: "hello",
+  data() {
+    return {
+      msg: "Hello Vite",
+    };
+  },
+  components: {
+    subtitle: subtitle,
+  },
+};
+</script>
